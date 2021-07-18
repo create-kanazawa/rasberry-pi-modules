e13fe7bd33418178945e120dd686c69c3e0a90f6
create-kanazawa <85425946+create-kanazawa@users.noreply.github.com>
2021-07-18 17:02:01 +0900

    Update setup.sh

--- python_controller/shell/setup.sh
+++ python_controller/shell/setup.sh
@@ -8,22 +8,22 @@ sudo service vsftpd start
 pip install watchdog==0.10.6
 
 #PHP server setup
-sudo apt update
-sudo apt install apache2 -y
-sudo apt install php libapache2-mod-php -y
-sudo apt install apache2 -y
-sudo service apache2 start
+#sudo apt update
+#sudo apt install apache2 -y
+#sudo apt install php libapache2-mod-php -y
+#sudo apt install apache2 -y
+#sudo service apache2 start
 
 #move default file to /var/www/scripts/
-PYTHON_FOLDER=scripts
-cd /var/www/
-mkdir $PYTHON_FOLDER
-cp $SCRIPT_DIR/../php/file_upload.php ./
-cp $SCRIPT_DIR/../html/send_form.html ./
-cp $SCRIPT_DIR/../scripts/main.py ./$PYTHON_FOLDER
-cp $SCRIPT_DIR/../scripts/start.py ./$PYTHON_FOLDER
-cp $SCRIPT_DIR/../scripts/stop.py ./$PYTHON_FOLDER
-cp $SCRIPT_DIR/../scripts/reset.py ./$PYTHON_FOLDER
+#PYTHON_FOLDER=scripts
+#cd /var/www/
+#mkdir $PYTHON_FOLDER
+#cp $SCRIPT_DIR/../php/file_upload.php ./
+#cp $SCRIPT_DIR/../html/send_form.html ./
+#cp $SCRIPT_DIR/../scripts/main.py ./$PYTHON_FOLDER
+#cp $SCRIPT_DIR/../scripts/start.py ./$PYTHON_FOLDER
+#cp $SCRIPT_DIR/../scripts/stop.py ./$PYTHON_FOLDER
+#cp $SCRIPT_DIR/../scripts/reset.py ./$PYTHON_FOLDER
 
 # set chmod of /var/www/
-sudo chmod -R 777 /var/www/
+#sudo chmod -R 777 /var/www/
