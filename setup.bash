462953f98f024eea84bdfd83a8c91e5f5fac7f5b
MiyakeYuuki <research.myky@gmail.com>
2021-11-24 21:23:07 +0900

    update setup.bash modify bug

--- setup.bash
+++ setup.bash
@@ -1,4 +1,4 @@
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
-bash $SCRIPT_DIR/python_controller/shell/setup.bash
+bash $SCRIPT_DIR/python_controller/shell/setup.sh
 bash $SCRIPT_DIR/wheel_robot/setting/setup.bash
 bash $SCRIPT_DIR/robot_arm/setting/setup.bash
