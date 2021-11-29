1581412618550726c963ef02318df95a21c6c4ef
A-Kawakubo <atsushi5382kawa@gmail.com>
2021-11-29 23:56:06 +0900

    update UDP header

--- python_controller/scripts/header/UDP.py
+++ python_controller/scripts/header/UDP.py
@@ -6,6 +6,12 @@ client=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 server=socket.socket(socket.AF_INET,type=socket.SOCK_DGRAM)
 server.bind(('',recieve_port))
 
+
+def recieve_data():
+    print('wait for data...')
+    rx_message,addr=server.recvfrom(M_SIZE)
+    return rx_message.decode('utf-8')
+
 #send block --> input IP and message
 #message='abcedf'
 #IP='127.0.0.1'
