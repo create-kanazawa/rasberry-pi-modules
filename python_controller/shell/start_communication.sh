dddd5b9469653324a76d1384fa6cee431c02ef27
create-kanazawa <85425946+create-kanazawa@users.noreply.github.com>
2021-12-11 00:36:31 +0900

    Update start_communication.sh

--- python_controller/shell/start_communication.sh
+++ python_controller/shell/start_communication.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
-cd $SCRIPT_DIR/../
+
+TIME=$(date)
+echo "LOG is start from:"$TIME"-----------"
+
 #sync git
 echo 'waiting for git update...'
 sleep 5
 git pull
+
 #execute FTP
-python3 processControlMain.py
+python3 $SCRIPT_DIR/../processControlMain.py &
