5f0ef1a3c814cba95c0d631491316f3472a5b22c
MiyakeYuuki <77323541+MiyakeYuuki@users.noreply.github.com>
2021-12-10 23:39:01 +0900

    Update footer.py

--- robot_arm/setting/footer.py
+++ robot_arm/setting/footer.py
@@ -1,2 +1,2 @@
 #-----------------------robot_arm fotter-----------------------
-initial_servo();pi.set_mode(servo_pin1, pigpio.INPUT);pi.set_mode(servo_pin2, pigpio.INPUT);pi.set_mode(servo_pin3, pigpio.INPUT);pi.stop();
+#initial_servo();pi.set_mode(servo_pin1, pigpio.INPUT);pi.set_mode(servo_pin2, pigpio.INPUT);pi.set_mode(servo_pin3, pigpio.INPUT);pi.stop();
