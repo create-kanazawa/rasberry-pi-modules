fc39ce0c4ab28c24ebcab75f93d972871e112737
Atsushi Kawakubo <63512586+Kawakubo-A@users.noreply.github.com>
2021-12-11 19:50:05 +0900

    Update setup.bash

--- robot_arm/setting/setup.bash
+++ robot_arm/setting/setup.bash
@@ -2,7 +2,7 @@
 #########
 sudo sh -c 'echo 127.0.1.1 $(hostname) >> /etc/hosts'
 #
-echo 'please input IP address (ex: 192.168.100.***):' 
+echo 'please input IP address (ex: 192.168.100.***):'
 #
 read ip
 #
@@ -19,14 +19,11 @@ sudo echo 'interface wlan0'>>$TARGET_CONF
 sudo echo 'static ip_address=192.168.100.140/24'>>$TARGET_CONF
 sudo echo 'static routers=192.168.100.1'>>$TARGET_CONF
 sudo echo 'static domain_name_servers=192.168.100.1'>>$TARGET_CONF
-
 #########
 sudo raspi-config nonint do_i2c 0
 sudo raspi-config nonint do_spi 0
-
 #########
 sudo raspi-config nonint do_hostname steam-lab-99
-
 #########
 sudo apt-get install python3-numpy
 sudo apt-get install python3-setuptools
@@ -34,6 +31,5 @@ sudo apt-get install python3-pip
 sudo apt-get install python3-pip
 sudo apt-get install git git-core
 sudo pip -y install adafruit-pca9685
-
 #########
 echo 'dtparam=i2c_baudrate=10000' | sudo tee -a /boot/config.txt>/dev/null
