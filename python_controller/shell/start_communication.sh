ad7253ecb85fbe82e34942c95fdb431185d14dbe
Atsushi Kawakubo <63512586+Kawakubo-A@users.noreply.github.com>
2021-12-11 17:44:42 +0900

    Update start_communication.sh

--- python_controller/shell/start_communication.sh
+++ python_controller/shell/start_communication.sh
@@ -7,6 +7,9 @@ echo "LOG is start from:"$TIME"-----------"
 #sync git
 echo 'waiting for git update...'
 sleep 5
+_IP=$(hostname -I) || true
+if [ "$_IP" ]; then
+  echo $_IP
 git pull
 
 #execute FTP
