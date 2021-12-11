a4a985f9f59bf4524571bf7c024ed6cf321412f5
Atsushi Kawakubo <63512586+Kawakubo-A@users.noreply.github.com>
2021-12-11 17:34:28 +0900

    Update setup.bash
    add the  robot setting bash

---
+++ robot_arm/setting/setup.bash
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+########固定IPアドレスの設定を行う
+#端末からIPアドレスを指定する
+echo -n please input IP address (ex: 192.168.100.***): 
+#入力を受付、その入力を「str」に代入
+read ip 
+#結果を表示
+echo IP address is set to $ip
+#設定ファイルに書き込みする
+TARGET_CONF=/etc/dhcpcd.conf
+echo 'interface wlan0' | sudo tee -a TARGET_CONF>/dev/null
+echo 'static ip_address='$ip | sudo tee -a TARGET_CONF>/dev/null
+echo 'static routers=192.168.100.1' | sudo tee -a TARGET_CONF>/dev/null
+echo 'static domain_name_servers=192.168.100.1' | sudo tee -a TARGET_CONF>/dev/null
+
+#########I2C通信の設定
+sudo raspi-config nonint do_i2c 0
+sudo raspi-config nonint do_spi 0
+
+#########ホストネームの変更
+sudo raspi-config nonint do_hostname steam-lab-99
+
+#########モジュールのインストール
+sudo apt-get -y install　python3-numpy
+sudo apt-get -y install　python3-setuptools
+sudo apt-get -y install　python3-pip
+sudo apt-get -y install　python3-pip
+sudo apt-get -y install git git-core
+sudo pip -y install adafruit-pca9685
+
+#########I2C通信の設定を通信速度を設定する
+echo 'dtparam=i2c_baudrate=10000' | sudo tee -a /boot/config.txt>/dev/null
+
+#########GPIOの設定(今は不要)
+#sudo apt install pigpio python-pigpio python3-
