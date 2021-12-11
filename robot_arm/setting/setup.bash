4fb9d249636ef00fb4ace6730715bdbfc227d79a
Atsushi Kawakubo <63512586+Kawakubo-A@users.noreply.github.com>
2021-12-11 20:19:14 +0900

    Update setup.bash

--- robot_arm/setting/setup.bash
+++ robot_arm/setting/setup.bash
@@ -10,16 +10,16 @@ sudo sh -c 'echo 127.0.1.1 $(hostname) >> /etc/hosts'
 #echo 'IP address is set to '$str
 #
 TARGET_CONF=/etc/dhcpcd.conf
-if [ ! -e $TARGET_CONF'-default' ];then
-	sudo cp $TARGET_CONF $TARGET_CONF'-default'
-fi
+#if [ ! -e $TARGET_CONF'-default' ];then
+sudo cp $TARGET_CONF $TARGET_CONF'-default'
+#fi
 TIME=$(date)
 echo "#-----------change by setup.bash: "$TIME"-----------" | sudo tee -a $TARGET_CONF>/dev/null
-sudo echo 'interface wlan0' | sudo tee -a $TARGET_CONF>/dev/null
+echo 'interface wlan0' | sudo tee -a $TARGET_CONF>/dev/null
 #sudo echo 'static ip_address='$ip'/24' | sudo tee -a $TARGET_CONF>/dev/null
-sudo echo 'static ip_address=192.168.100.140/24' | sudo tee -a $TARGET_CONF>/dev/null
-sudo echo 'static routers=192.168.100.1' | sudo tee -a $TARGET_CONF>/dev/null
-sudo echo 'static domain_name_servers=192.168.100.1' | sudo tee -a $TARGET_CONF>/dev/null
+echo 'static ip_address=192.168.100.140/24' | sudo tee -a $TARGET_CONF>/dev/null
+echo 'static routers=192.168.100.1' | sudo tee -a $TARGET_CONF>/dev/null
+echo 'static domain_name_servers=192.168.100.1' | sudo tee -a $TARGET_CONF>/dev/null
 #########
 sudo raspi-config nonint do_i2c 0
 sudo raspi-config nonint do_spi 0
