885d44c034a86ffddfcb5cbf321ba4f20acaa86f
Atsushi Kawakubo <63512586+Kawakubo-A@users.noreply.github.com>
2021-12-22 08:46:49 +0900

    Update setup.bash

--- robot_arm/setting/setup.bash
+++ robot_arm/setting/setup.bash
@@ -1,30 +1,19 @@
 #!/bin/bash
 #########
-#sudo sh -c 'echo 127.0.1.1 $(hostname) >> /etc/hosts'
-#
-#echo 'please input IP address (ex. 192.168.100.***)'
-#
-#str='A'
-#read ip
-#
-#echo 'IP address is set to '$ip
-#
-TARGET_CONF=/etc/dhcpcd.conf
-#if [ ! -e $TARGET_CONF'-default' ];then
-sudo cp $TARGET_CONF $TARGET_CONF'-default'
-#fi
-TIME=$(date)
-echo "#-----------change by setup.bash: "$TIME"-----------" | sudo tee -a $TARGET_CONF>/dev/null
-echo 'interface wlan0' | sudo tee -a $TARGET_CONF>/dev/null
+#TARGET_CONF=/etc/dhcpcd.conf
+#sudo cp $TARGET_CONF $TARGET_CONF'-default'
+#TIME=$(date)
+#echo "#-----------change by setup.bash: "$TIME"-----------" | sudo tee -a $TARGET_CONF>/dev/null
+#echo 'interface wlan0' | sudo tee -a $TARGET_CONF>/dev/null
 #sudo echo 'static ip_address='$ip'/24' | sudo tee -a $TARGET_CONF>/dev/null
-echo 'static ip_address=192.168.100.140/24' | sudo tee -a $TARGET_CONF>/dev/null
-echo 'static routers=192.168.100.1' | sudo tee -a $TARGET_CONF>/dev/null
-echo 'static domain_name_servers=192.168.100.1' | sudo tee -a $TARGET_CONF>/dev/null
+#echo 'static ip_address=192.168.100.140/24' | sudo tee -a $TARGET_CONF>/dev/null
+#echo 'static routers=192.168.100.1' | sudo tee -a $TARGET_CONF>/dev/null
+#echo 'static domain_name_servers=192.168.100.1' | sudo tee -a $TARGET_CONF>/dev/null
 #########
 sudo raspi-config nonint do_i2c 0
 sudo raspi-config nonint do_spi 0
 #########
-sudo raspi-config nonint do_hostname steam-lab-99
+#sudo raspi-config nonint do_hostname steam-lab-99
 #########
 sudo apt-get install python3-numpy
 sudo apt-get install python3-setuptools
