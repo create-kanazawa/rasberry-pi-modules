c5701685ab0e24e25dd7d1851d34fe179e78d7c4
create-kanazawa <85425946+create-kanazawa@users.noreply.github.com>
2021-12-22 21:23:27 +0900

    Update setup.bash

--- setup.bash
+++ setup.bash
@@ -1,7 +1,19 @@
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 ######apt update upgrade######
-sudo apt update
-sudo apt upgrade
+sudo apt update -y
+sudo apt upgrade -y
+
+######setting i2c spi######
+sudo raspi-config nonint do_i2c 0
+sudo raspi-config nonint do_spi 0
+
+######install module######
+sudo apt install -y expect
+sudo apt install -y python3-numpy
+sudo apt install -y python3-setuptools
+sudo apt install -y python3-pip
+sudo pip3 install adafruit-pca9685
+#########
 bash $SCRIPT_DIR/python_controller/shell/setup.sh
 bash $SCRIPT_DIR/wheel_robot/setting/setup.bash
 bash $SCRIPT_DIR/robot_arm/setting/setup.bash
