94d1917e88c199c24d9083e822c9dcb48f897bfc
create-kanazawa <85425946+create-kanazawa@users.noreply.github.com>
2021-12-22 20:10:22 +0900

    Update setup.bash

--- setup.bash
+++ setup.bash
@@ -1,4 +1,4 @@
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
-sudo bash $SCRIPT_DIR/python_controller/shell/setup.sh
-sudo bash $SCRIPT_DIR/wheel_robot/setting/setup.bash
-sudo bash $SCRIPT_DIR/robot_arm/setting/setup.bash
+bash $SCRIPT_DIR/python_controller/shell/setup.sh
+bash $SCRIPT_DIR/wheel_robot/setting/setup.bash
+bash $SCRIPT_DIR/robot_arm/setting/setup.bash
