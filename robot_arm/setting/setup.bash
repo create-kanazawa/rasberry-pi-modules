11a3717565db581d2a55104a19c888f4f19490a2
create-kanazawa <85425946+create-kanazawa@users.noreply.github.com>
2021-12-22 21:17:53 +0900

    Update setup.bash

--- robot_arm/setting/setup.bash
+++ robot_arm/setting/setup.bash
@@ -1,15 +1,15 @@
 #!/bin/bash
 #########
-sudo apt update
-sudo apt upgrade
-sudo apt -y  install expect
+#sudo apt update
+#sudo apt upgrade
+#sudo apt -y  install expect
 #########
-sudo raspi-config nonint do_i2c 0
-sudo raspi-config nonint do_spi 0
+#sudo raspi-config nonint do_i2c 0
+#sudo raspi-config nonint do_spi 0
 #########
-sudo apt install -y python3-numpy
-sudo apt install -y python3-setuptools
-sudo apt install -y python3-pip
-sudo pip3 install adafruit-pca9685
+#sudo apt install -y python3-numpy
+#sudo apt install -y python3-setuptools
+#sudo apt install -y python3-pip
+#sudo pip3 install adafruit-pca9685
 #########
 #echo 'dtparam=i2c_baudrate=10000' | sudo tee -a /boot/config.txt>/dev/null
